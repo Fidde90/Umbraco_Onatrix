54e10a6b3d55717e9170e1d94d3cbedb4539f72f
Fredrik <fredrik.t.bengtsson@hotmail.com>
2024-09-27 19:44:17 +0200

    Css form errors and success done

--- Umbraco_Onatrix/Controllers/ServicesController.cs
+++ Umbraco_Onatrix/Controllers/ServicesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
 using Umbraco.Cms.Core.Routing;
@@ -6,6 +7,7 @@ using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco_Onatrix.Models;
 
 namespace Umbraco_Onatrix.Controllers
 {
@@ -18,9 +20,28 @@ namespace Umbraco_Onatrix.Controllers
 
         }
 
-        public IActionResult Index()
+        public IActionResult SubmitHandler(ServiceQuestionFormModel form)
         {
-            return View();
+            //    Regex emailRegEx = new("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+            //    Regex nameRegEx = new("^[\\p{L}\\p{M}\\'\\-\\. ]+$");
+
+            //    Match emailResult = emailRegEx.Match(form.Email);
+            //    Match nameResult = nameRegEx.Match(form.Name);
+
+            if (!ModelState.IsValid)
+            {
+
+                TempData["error_name"] = "fail";
+                TempData["error_email"] = "fail";
+                TempData["error_message"] = "fail";
+
+                TempData["name"] = form.Name;
+                TempData["email"] = form.Email;
+                TempData["message"] = form.Message;
+
+                return Redirect(UmbracoContext.OriginalRequestUrl.ToString() + "#service-form");
+            }
+            return Redirect(UmbracoContext.OriginalRequestUrl.ToString() + "#service-form");
         }
     }
 }
--- Umbraco_Onatrix/Models/ServiceQuestionFormModel.cs
+++ Umbraco_Onatrix/Models/ServiceQuestionFormModel.cs
@@ -1,12 +1,15 @@
-﻿namespace Umbraco_Onatrix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Umbraco_Onatrix.Models
 {
     public class ServiceQuestionFormModel
     {
         public string Name { get; set; } = null!;
 
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
         public string Email { get; set; } = null!;
 
         public string Message { get; set; } = null!;
-
     }
 }
